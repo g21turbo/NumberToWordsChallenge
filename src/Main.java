c31284e2c62b69b9b69c12261e86e4763d1c1690
g21turbo <dg21stew@gmail.com>
2023-11-07 22:01:51 -0800

    added reverse and digitCount methods

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,58 @@
 public class Main {
     public static void main(String[] args) {
 
+        numberToWords(1294);
 
     }
 
-    public static void numberToWords(int number) {
+    public static int numberToWords(int number) {
 
         // input validation
         if (number < 0) {
             System.out.println("Invalid Value");
         }
 
-        int lastDigit = number % 10;
-        if (lastDigit == 0) {
-            System.out.println("Zero");
-        } else if (lastDigit == 1) {
-            System.out.println("One");
-        } else if (lastDigit == 2) {
-            System.out.println("Two");
-        } else if (lastDigit == 3) {
-            System.out.println("Three");
-        } else if (lastDigit == 4) {
-            System.out.println("Four");
-        } else if (lastDigit == 5) {
-            System.out.println("Five");
-        } else if (lastDigit == 6) {
-            System.out.println("Six");
-        } else if (lastDigit == 7) {
-            System.out.println("Seven");
-        } else if (lastDigit == 8) {
-            System.out.println("Eight");
-        } else {
-            System.out.println("Nine");
+        for (int i = 0; i <= number; i++) {
+
+            int lastDigit = number % 10;
+
+
+            if (lastDigit == 0) {
+                System.out.println("Zero");
+            } else if (lastDigit == 1) {
+                System.out.println("One");
+            } else if (lastDigit == 2) {
+                System.out.println("Two");
+            } else if (lastDigit == 3) {
+                System.out.println("Three");
+            } else if (lastDigit == 4) {
+                System.out.println("Four");
+            } else if (lastDigit == 5) {
+                System.out.println("Five");
+            } else if (lastDigit == 6) {
+                System.out.println("Six");
+            } else if (lastDigit == 7) {
+                System.out.println("Seven");
+            } else if (lastDigit == 8) {
+                System.out.println("Eight");
+            } else {
+                System.out.println("Nine");
+            }
+            number = number / 10;
+
         }
+        return reverse(number);
+    }
+
+    public static int reverse(int number) {
 
     }
 
+    public static int digitCount(int number) {
+
+        if (number < 0) {
+            return -1;
+        }
+    }
 
 }
