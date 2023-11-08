ebe448d024661071fc8ecfd223ce5cb391ff97de
g21turbo <dg21stew@gmail.com>
2023-11-07 21:33:42 -0800

    lots of el-if statements

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,29 @@ public class Main {
             System.out.println("Invalid Value");
         }
 
+        int lastDigit = number % 10;
+        if (lastDigit == 0) {
+            System.out.println("Zero");
+        } else if (lastDigit == 1) {
+            System.out.println("One");
+        } else if (lastDigit == 2) {
+            System.out.println("Two");
+        } else if (lastDigit == 3) {
+            System.out.println("Three");
+        } else if (lastDigit == 4) {
+            System.out.println("Four");
+        } else if (lastDigit == 5) {
+            System.out.println("Five");
+        } else if (lastDigit == 6) {
+            System.out.println("Six");
+        } else if (lastDigit == 7) {
+            System.out.println("Seven");
+        } else if (lastDigit == 8) {
+            System.out.println("Eight");
+        } else {
+            System.out.println("Nine");
+        }
+
     }
 
 
