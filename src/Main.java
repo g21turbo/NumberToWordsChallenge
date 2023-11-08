7a8be8ba69653f61f5bfadc3b3a2c87f56fbe32a
g21turbo <dg21stew@gmail.com>
2023-11-07 21:27:23 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void numberToWords(int number) {
+
+        // input validation
+        if (number < 0) {
+            System.out.println("Invalid Value");
+        }
+
+    }
+
+
+}
